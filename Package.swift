5de065030e04275a12e8a4043f9f78c00e837397
Kartheek Solipuram <kartheek.s@gmail.com>
2020-10-25 21:09:44 -0700

    Update dependency ref to val-verde.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pvieito/LoggerKit.git", .branch("master")),
-        .package(url: "https://github.com/apple/swift-argument-parser", .branch("main")),
+        .package(url: "https://github.com/val-verde/swift-argument-parser", .branch("val-verde-mainline")),
     ],
     targets: [
         .target(
